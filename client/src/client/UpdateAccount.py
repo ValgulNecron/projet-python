eb4fe08836f1d500162ce55254cfe524d98759ff
MisterEggs <estephe.caret@gmail.com>
2024-02-29 15:08:22 +0100

    Premier commit

---
+++ client/src/client/UpdateAccount.py
@@ -0,0 +1,12 @@
+class CreateAccount :
+    id = ""
+    token = ""
+    email = ""
+    password = ""
+    username = ""
+    def __init__ (self, id, token, email, password, username) :
+        self.id = id
+        self.token = token
+        self.email = email
+        self.password = password
+        self.username = username
