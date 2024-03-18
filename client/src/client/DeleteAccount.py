b2dcfee8b2b5feb767d2e0034dad5281aaba3af8
EstepheC <estephe.caret@gmail.com>
2024-03-18 16:47:06 +0100

    Ajout de la fonction delete_account

--- client/src/client/DeleteAccount.py
+++ client/src/client/DeleteAccount.py
@@ -1,3 +1,7 @@
+import grpc
+from client.src.client.proto_compiled.account import account_pb2_grpc, account_pb2
+
+
 class DeleteAccount:
     id = ""
     token = ""
@@ -5,3 +9,8 @@ class DeleteAccount:
     def __init__(self, id, token):
         self.id = id
         self.token = token
+    def delete_account(self):
+        with grpc.insecure_channel("141.145.209.36:3333") as channel:
+            stub = account_pb2_grpc.AccountStub(channel)
+            response = stub.DeleteAccount(account_pb2.DeleteAccountRequest(id=self.id,token=self.token))
+        print("client received: " + str(response.deleted) + ' ' + response.id)
