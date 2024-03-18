d487fd5d1705c5e21a2e05c3c220f92a62889ebf
valgul <valgulnecron@gmail.com>
2024-03-18 18:23:05 +0100

    Temp off.

--- server/src/main.rs
+++ server/src/main.rs
@@ -21,22 +21,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         users_token: account_token.clone(),
     };
 
-    let map = load_all_item_from_json();
-    let data_service = DataService {
+    //let map = load_all_item_from_json();
+    /*let data_service = DataService {
         users_token: account_token.clone(),
         items: map,
-    };
+    };*/
 
     let reflection = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(service::account_services::proto::FILE_DESCRIPTOR_SET)
-        .register_encoded_file_descriptor_set(service::data_services::proto::FILE_DESCRIPTOR_SET)
+        //.register_encoded_file_descriptor_set(service::data_services::proto::FILE_DESCRIPTOR_SET)
         .build()?;
     Server::builder()
         .add_service(reflection)
         .add_service(get_account_service(account_service))
-        .add_service(get_item_service(data_service.clone()))
+        /*.add_service(get_item_service(data_service.clone()))
         .add_service(get_map_service(data_service.clone()))
-        .add_service(get_user_service(data_service))
+        .add_service(get_user_service(data_service))*/
         .serve(addr)
         .await?;
     println!("Server running on {}", addr);
