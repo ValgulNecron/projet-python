992b25ad217181dd2a9cf1765cc94b357090fc71
valgul <valgulnecron@gmail.com>
2024-03-19 18:36:46 +0100

    fix message

--- server/src/service/account_services.rs
+++ server/src/service/account_services.rs
@@ -139,13 +139,13 @@ impl Account for AccountService {
         let data = request.into_inner();
         let row = match get_account_by_username(data.username).await {
             Some(row) => row,
-            None => return Err(Status::unauthenticated("Invalid password or username")),
+            None => return Err(Status::unauthenticated("Invalid password or username 1012")),
         };
         let password: String = row.get(3);
         let same_password = verify_password(data.password.as_ref(), password.as_str());
         let id: String = row.get(0);
         if !same_password {
-            return Err(Status::unauthenticated("Invalid password or username"));
+            return Err(Status::unauthenticated("Invalid password or username 1013"));
         }
 
         let token = Uuid::new_v4().to_string();
